9fcabe08d32230ec220d2b64e15c684a94a4b846
The_Potato <134632740+CaiqueGioppo@users.noreply.github.com>
2024-04-04 10:53:02 -0300

    carro.py foi alterado.

---
+++ carro.py
@@ -0,0 +1,11 @@
+class Carro:0
+def __init__ (self, marca, modelo, ano):
+    self.marca = marca
+    self.modelo = modelo
+    self.ano = ano
+
+def descricao(self):
+    return f"{self.marca} {self.modelo} {self.ano}"
+
+def dirigir(self):
+    return f"{self.marca} {self.modelo} está em movimento"
