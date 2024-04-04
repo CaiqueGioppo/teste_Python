51f2b0e138f22f8f520e3db14c20a785cc7feb8e
The_Potato <134632740+CaiqueGioppo@users.noreply.github.com>
2024-04-04 11:06:12 -0300

    main.py incompleto

---
+++ main.py
@@ -0,0 +1,8 @@
+fromm  carro import Carro
+
+#criando instância da classe Carro
+meu_carro = Carro("Ford", "Focus", 2020)
+meu_carro = Carro(" BYD", "Dolphin", 2024)
+
+#Imprimindo atributos e métodos do objeto meu_carro
+print
